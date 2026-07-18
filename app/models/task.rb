eb5487a8281c88dd8d2ada69c56e9fb690ef7abd
mize1978 <mizelate@gmail.com>
2026-07-18 20:09:23 +0900

    fix: add priority validation to Task model (high/medium/low)

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,10 @@
 class Task < ApplicationRecord
   belongs_to :user
 
+  PRIORITIES = %w[high medium low].freeze
   CATEGORIES = %w[学習 健康 習慣 家事 仕事 その他].freeze
+
+  validates :priority, inclusion: { in: PRIORITIES }, allow_nil: true
 
   def start_time
     date
